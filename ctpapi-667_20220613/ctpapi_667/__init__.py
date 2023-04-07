103187287766c115eb6591955283394e8880670c
Jedore <jedore@protonmail.com>
2023-04-07 20:03:15 +0800

    optimize

--- ctpapi-667_20220613/ctpapi_667/__init__.py
+++ ctpapi-667_20220613/ctpapi_667/__init__.py
@@ -1,14 +1,3 @@
 # SPDX-FileCopyrightText: 2023-present Jedore <jedore@protonmail.com>
 #
 # SPDX-License-Identifier: MIT
-import os
-
-# from . import thostmduserapi as MduserApi
-# from . import thosttraderapi as TraderApi
-#
-# __all__ = [
-#     'TraderApi',
-#     'MduserApi',
-# ]
-
-os.add_dll_directory(os.path.join(os.path.dirname(__file__), 'win64'))
--- ctpapi-667_20220613/hatch_build_hook.py
+++ ctpapi-667_20220613/hatch_build_hook.py
@@ -9,16 +9,16 @@ class CustomBuildHook(BuildHookInterface):
         build_data['pure_python'] = False
         build_data['infer_tag'] = True
         if sys.platform.startswith('linux'):
-            pass
+            build_data['artifacts'] = ['ctpapi_667/linux64/*.so']
         elif sys.platform.startswith('darwin'):
-            build_data['artifacts'] = [
-                'ctpapi_667/mac64/*.so',
-            ]
+            build_data['artifacts'] = ['ctpapi_667/mac64/*.so']
         elif sys.platform.startswith('win'):
             major, minor = sys.version_info[:2]
             assert major == 3
             assert minor in (7, 8, 9, 10, 11)
-            build_data['artifacts'] = [
-                'ctpapi_667/win64/*.dll',
-                f'ctpapi_667/win64/py3{minor}',
-            ]
+            build_data['force_include'].update({
+                'ctpapi_667/win64/thostmduserapi_se.dll': 'ctpapi_667/thostmduserapi_se.dll',
+                'ctpapi_667/win64/thosttraderapi_se.dll': 'ctpapi_667/thosttraderapi_se.dll',
+                f'ctpapi_667/win64/py3{minor}/_thostmduserapi.pyd': 'ctpapi_667/thostmduserapi.pyd',
+                f'ctpapi_667/win64/py3{minor}/_thosttraderapi.pyd': 'ctpapi_667/thosttraderapi.pyd',
+            })
