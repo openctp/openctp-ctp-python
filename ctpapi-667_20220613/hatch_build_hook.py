f1e3397d96db4a9e307c3a9a23938f5d154e70fe
Jedore <jedore@protonmail.com>
2023-04-08 12:29:45 +0800

    optimize

--- ctpapi-667_20220613/hatch_build_hook.py
+++ ctpapi-667_20220613/hatch_build_hook.py
@@ -8,6 +8,7 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         build_data['pure_python'] = False
+        build_data['infer_tag'] = True
         if sys.platform.startswith('linux'):
             build_data['artifacts'].extend([
                 'ctpapi_667/linux-x86_64/*.so',
@@ -22,7 +23,6 @@ class CustomBuildHook(BuildHookInterface):
             major, minor = sys.version_info[:2]
             assert major == 3
             assert minor in (7, 8, 9, 10, 11)
-            build_data['infer_tag'] = True
             build_data['artifacts'].append('ctpapi_667/thosttraderapi.py')
             build_data['force_include'].update({
                 'ctpapi_667/win64/thostmduserapi_se.dll': 'ctpapi_667/thostmduserapi_se.dll',
