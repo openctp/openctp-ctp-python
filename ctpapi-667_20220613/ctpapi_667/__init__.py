a2f7f34d218cf2e3705365f7484e79ae19bd4751
Jedore <jedore@protonmail.com>
2023-04-09 22:21:46 +0800

    optimize

--- ctpapi-667_20220613/ctpapi_667/__init__.py
+++ ctpapi-667_20220613/ctpapi_667/__init__.py
@@ -1,3 +1,19 @@
 # SPDX-FileCopyrightText: 2023-present Jedore <jedore@protonmail.com>
 #
 # SPDX-License-Identifier: MIT
+import os
+import sys
+
+path = os.getcwd()
+
+is_linux = sys.platform.startswith('linux')
+if is_linux:
+    os.chdir(os.path.dirname(__file__))
+
+from . import thostmduserapi as mdapi
+from . import thosttraderapi as tdapi
+
+if is_linux:
+    os.chdir(path)
+
+__all__ = ['mdapi', 'tdapi']
