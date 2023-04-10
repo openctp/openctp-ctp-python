c622c91ae5dc143a72adb735ed53811984cd3400
Jedore <jedore@protonmail.com>
2023-04-11 03:16:08 +0800

    fix

--- ctpapi-667/tests/pre_test.py
+++ ctpapi-667/tests/pre_test.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import platform
 import shutil
@@ -6,10 +7,13 @@ import sys
 cur_dir = os.path.dirname(__file__)
 old_dir = os.path.join(os.path.dirname(cur_dir), 'ctpapi_667')
 
+srcs = []
+srcs.extend(glob.glob(os.path.join(old_dir, 'thost*.py')))
 if sys.platform.startswith('linux'):
-    src = os.path.join(old_dir, 'linux_x64')
+    srcs.extend(glob.glob(os.path.join(old_dir, 'linux_x64', '*')))
 elif sys.platform.startswith('darwin'):
     if platform.machine() == 'x86_64':
+        srcs.extend(glob.glob(os.path.join(old_dir, 'mac_x64', '*')))
         src = os.path.join(old_dir, 'mac_x64')
     elif platform.machine() == 'amd64':
         pass
@@ -17,8 +21,8 @@ elif sys.platform.startswith('win'):
     major, minor = sys.version_info[:2]
     assert major == 3
     assert minor in (7, 8, 9, 10, 11)
-    src = os.path.join(old_dir, 'win_x64', f'py{major}{minor}')
+    path = os.path.join(old_dir, 'win_x64', f'py{major}{minor}', '*')
+    srcs.extend(glob.glob(path))
 
-shutil.copy(os.path.join(old_dir, 'thostmduserapi.py'), cur_dir)
-shutil.copy(os.path.join(old_dir, 'thosttraderapi.py'), cur_dir)
-shutil.copytree(src, cur_dir, dirs_exist_ok=True)
+for src in srcs:
+    shutil.copy2(src, cur_dir)
