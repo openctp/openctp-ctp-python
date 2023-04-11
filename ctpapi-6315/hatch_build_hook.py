448911cb76b03c6520b8a383579cfc223cb71fc7
Jedore <jedore@protonmail.com>
2023-04-12 01:54:56 +0800

    complete yml; refactor directory

--- ctpapi-6315/hatch_build_hook.py
+++ ctpapi-6315/hatch_build_hook.py
@@ -9,24 +9,27 @@ class CustomBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: Dict[str, Any]) -> None:
         build_data['pure_python'] = False
         build_data['infer_tag'] = True
+
         if sys.platform.startswith('linux'):
-            build_data['artifacts'].extend([
-                'ctpapi_6315/linux_x64/*.so',
-                'ctpapi_6315/thosttraderapi.py',
-            ])
+            build_data['force_include'].update({
+                '6315/linux_x64': 'ctpapi_6315',
+            })
+
         elif sys.platform.startswith('darwin'):
             if platform.machine() == 'x86_64':
-                build_data['artifacts'].append('ctpapi_6315/mac_x64/*.so')
+                build_data['force_include'].update({
+                    '6315/mac_x64': 'ctpapi_6315',
+                })
             elif platform.machine() == 'amd64':
                 pass
+
         elif sys.platform.startswith('win'):
             major, minor = sys.version_info[:2]
             assert major == 3
             assert minor in (7, 8, 9, 10, 11)
-            build_data['artifacts'].append('ctpapi_6315/thosttraderapi.py')
             build_data['force_include'].update({
-                'ctpapi_6315/win_x64/thostmduserapi_se.dll': 'ctpapi_6315/thostmduserapi_se.dll',
-                'ctpapi_6315/win_x64/thosttraderapi_se.dll': 'ctpapi_6315/thosttraderapi_se.dll',
-                f'ctpapi_6315/win_x64/py3{minor}/_thostmduserapi.pyd': 'ctpapi_6315/_thostmduserapi.pyd',
-                f'ctpapi_6315/win_x64/py3{minor}/_thosttraderapi.pyd': 'ctpapi_6315/_thosttraderapi.pyd',
+                '6315/win_x64/thostmduserapi_se.dll': 'ctpapi_6315/thostmduserapi_se.dll',
+                '6315/win_x64/thosttraderapi_se.dll': 'ctpapi_6315/thosttraderapi_se.dll',
+                f'6315/win_x64/py3{minor}/_thostmduserapi.pyd': 'ctpapi_6315/_thostmduserapi.pyd',
+                f'6315/win_x64/py3{minor}/_thosttraderapi.pyd': 'ctpapi_6315/_thosttraderapi.pyd',
             })
--- ctpapi-669/hatch_build_hook.py
+++ ctpapi-669/hatch_build_hook.py
@@ -9,24 +9,27 @@ class CustomBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: Dict[str, Any]) -> None:
         build_data['pure_python'] = False
         build_data['infer_tag'] = True
+
         if sys.platform.startswith('linux'):
-            build_data['artifacts'].extend([
-                'ctpapi_669/linux_x64/*.so',
-                'ctpapi_669/thosttraderapi.py',
-            ])
+            build_data['force_include'].update({
+                '669/linux_x64': 'ctpapi_669',
+            })
+
         elif sys.platform.startswith('darwin'):
             if platform.machine() == 'x86_64':
-                build_data['artifacts'].append('ctpapi_669/mac_x64/*.so')
+                build_data['force_include'].update({
+                    '669/mac_x64': 'ctpapi_669',
+                })
             elif platform.machine() == 'amd64':
                 pass
+
         elif sys.platform.startswith('win'):
             major, minor = sys.version_info[:2]
             assert major == 3
             assert minor in (7, 8, 9, 10, 11)
-            build_data['artifacts'].append('ctpapi_669/thosttraderapi.py')
             build_data['force_include'].update({
-                'ctpapi_669/win_x64/thostmduserapi_se.dll': 'ctpapi_669/thostmduserapi_se.dll',
-                'ctpapi_669/win_x64/thosttraderapi_se.dll': 'ctpapi_669/thosttraderapi_se.dll',
-                f'ctpapi_669/win_x64/py3{minor}/_thostmduserapi.pyd': 'ctpapi_669/_thostmduserapi.pyd',
-                f'ctpapi_669/win_x64/py3{minor}/_thosttraderapi.pyd': 'ctpapi_669/_thosttraderapi.pyd',
+                '669/win_x64/thostmduserapi_se.dll': 'ctpapi_669/thostmduserapi_se.dll',
+                '669/win_x64/thosttraderapi_se.dll': 'ctpapi_669/thosttraderapi_se.dll',
+                f'669/win_x64/py3{minor}/_thostmduserapi.pyd': 'ctpapi_669/_thostmduserapi.pyd',
+                f'669/win_x64/py3{minor}/_thosttraderapi.pyd': 'ctpapi_669/_thosttraderapi.pyd',
             })
