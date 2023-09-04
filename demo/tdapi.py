6eeea5896da435a68b19cbdaddc6e62498f58929
Jedore <jedore@protonmail.com>
2023-09-04 11:11:39 +0800

    Optimize demo

--- demo/tdapi.py
+++ demo/tdapi.py
@@ -1,72 +1,263 @@
 """
     交易API demo
 """
-import random
+import inspect
+import time
 
 from openctp_ctp import tdapi
 
+import config
+
 
 class CTdSpiImpl(tdapi.CThostFtdcTraderSpi):
-    def __init__(self, tdapi):
+    """交易回调实现类"""
+
+    def __init__(
+            self,
+            front: str,
+            user: str,
+            passwd: str,
+            authcode: str,
+            appid: str,
+            broker_id: str,
+    ):
+        print("-------------------------------- 启动 trader api demo ")
         super().__init__()
-        self.tdapi = tdapi
+        self._front = front
+        self._user = user
+        self._password = passwd
+        self._authcode = authcode
+        self._appid = appid
+        self._broker_id = broker_id
+
+        self._is_authenticate = False
+        self._is_login = False
+        self._is_last = True
+        self._print_max = 5
+        self._print_count = 0
+
+        self._api: tdapi.CThostFtdcTraderApi = (
+            tdapi.CThostFtdcTraderApi.CreateFtdcTraderApi(self._user)
+        )
+
+        print("CTP交易API版本号:", self._api.GetApiVersion())
+        print("交易前置:" + self._front)
+
+        # 注册交易前置
+        self._api.RegisterFront(self._front)
+        # 注册交易回调实例
+        self._api.RegisterSpi(self)
+        # 订阅私有流
+        self._api.SubscribePrivateTopic(tdapi.THOST_TERT_QUICK)
+        # 订阅公有流
+        self._api.SubscribePublicTopic(tdapi.THOST_TERT_QUICK)
+        # 初始化交易实例
+        self._api.Init()
+        print("初始化成功")
+
+    @property
+    def is_login(self):
+        return self._is_login
+
+    def release(self):
+        # 释放实例
+        print("释放实例")
+        self._api.Release()
+
+    def _check_req(self, req, ret: int):
+        """检查请求"""
+
+        # 打印请求
+        params = []
+        for name, value in inspect.getmembers(req):
+            if name[0].isupper():
+                params.append(f"{name}={value}")
+        self.print("发送请求:", ",".join(params))
+
+        # 检查请求结果
+        error = {
+            0: "",
+            -1: "网络连接失败",
+            -2: "未处理请求超过许可数",
+            -3: "每秒发送请求数超过许可数",
+        }.get(ret, "未知错误")
+        if ret != 0:
+            self.print(f"请求失败: {ret}={error}")
+
+    def _check_rsp(self, pRspInfo: tdapi.CThostFtdcRspInfoField, rsp=None, is_last: bool = True) -> bool:
+        """检查响应
+
+        True: 成功 False: 失败
+        """
+
+        if is_last:
+            self._is_last = True
+            self._print_count = 0
+
+            if pRspInfo and pRspInfo.ErrorID != 0:
+                self.print(
+                    f"响应失败, ErrorID={pRspInfo.ErrorID}, ErrorMsg={pRspInfo.ErrorMsg}"
+                )
+                return False
+
+            self.print(f"响应成功")
+            if rsp:
+                params = []
+                for name, value in inspect.getmembers(rsp):
+                    if name[0].isupper():
+                        params.append(f"{name}={value}")
+                self.print("响应内容:", ",".join(params))
+
+        else:
+            if self._print_count >= self._print_max:
+                return True
+            self._is_last = False
+            self._print_count += 1
+
+            if rsp:
+                params = []
+                for name, value in inspect.getmembers(rsp):
+                    if name[0].isupper():
+                        params.append(f"{name}={value}")
+                self.print("     ", ",".join(params))
+        return True
+
+    @staticmethod
+    def print(*args, **kwargs):
+        print("    ", *args, **kwargs)
 
     def OnFrontConnected(self):
-        """ 前置连接成功 """
-        print("OnFrontConnected")
-        req = tdapi.CThostFtdcReqAuthenticateField()
-        req.BrokerID = brokerid
-        req.UserID = user
-        req.AppID = appid
-        req.AuthCode = authcode
-        self.tdapi.ReqAuthenticate(req, 0)
+        """交易前置连接成功"""
+        print("交易前置连接成功")
+
+        self.authenticate()
 
     def OnFrontDisconnected(self, nReason: int):
-        """ 前置断开 """
-        print("OnFrontDisconnected: nReason=", nReason)
+        """交易前置连接断开"""
+        print("交易前置连接断开: nReason=", nReason)
 
-    def OnRspAuthenticate(self, pRspAuthenticateField: tdapi.CThostFtdcRspAuthenticateField,
-                          pRspInfo: tdapi.CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool):
-        """ 客户端认证应答 """
-        if pRspInfo is not None:
-            print(f"OnRspAuthenticate: ErrorID={pRspInfo.ErrorID}, ErrorMsg={pRspInfo.ErrorMsg}")
+        # todo 可以在这里定义交易连接断开时的逻辑
 
-        if pRspInfo is None or pRspInfo.ErrorID == 0:
-            req = tdapi.CThostFtdcReqUserLoginField()
-            req.BrokerID = brokerid
-            req.UserID = user
-            req.Password = password
-            req.UserProductInfo = "openctp"
-            self.tdapi.ReqUserLogin(req, 0)
+    def authenticate(self):
+        """认证 demo"""
+        print("> 认证")
+        _req = tdapi.CThostFtdcReqAuthenticateField()
+        _req.BrokerID = self._broker_id
+        _req.UserID = self._user
+        _req.AppID = self._appid
+        _req.AuthCode = self._authcode
+        self._check_req(_req, self._api.ReqAuthenticate(_req, 0))
 
-    def OnRspUserLogin(self, pRspUserLogin: tdapi.CThostFtdcRspUserLoginField,
-                       pRspInfo: tdapi.CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool):
-        """ 登录应答 """
-        print(f"OnRspUserLogin: ErrorID={pRspInfo.ErrorID}, ErrorMsg={pRspInfo.ErrorMsg}, "
-              f"TradingDay={pRspUserLogin.TradingDay}")
+    def OnRspAuthenticate(
+            self,
+            pRspAuthenticateField: tdapi.CThostFtdcRspAuthenticateField,
+            pRspInfo: tdapi.CThostFtdcRspInfoField,
+            nRequestID: int,
+            bIsLast: bool,
+    ):
+        """客户端认证响应"""
+        if not self._check_rsp(pRspInfo, pRspAuthenticateField):
+            return
+
+        self._is_authenticate = True
+
+        # 登录
+        self.login()
+
+    def login(self):
+        """登录 demo"""
+        print("> 登录")
+
+        _req = tdapi.CThostFtdcReqUserLoginField()
+        _req.BrokerID = self._broker_id
+        _req.UserID = self._user
+        _req.Password = self._password
+        self._check_req(_req, self._api.ReqUserLogin(_req, 0))
+
+    def OnRspUserLogin(
+            self,
+            pRspUserLogin: tdapi.CThostFtdcRspUserLoginField,
+            pRspInfo: tdapi.CThostFtdcRspInfoField,
+            nRequestID: int,
+            bIsLast: bool,
+    ):
+        """登录响应"""
+        if not self._check_rsp(pRspInfo, pRspUserLogin):
+            return
+
+        self._is_login = True
+
+    def settlement_info_confirm(self):
+        """投资者结算结果确认"""
+        print("> 投资者结算结果确认")
+
+        _req = tdapi.CThostFtdcSettlementInfoConfirmField()
+        _req.BrokerID = self._broker_id
+        _req.InvestorID = self._user
+        self._check_req(_req, self._api.ReqSettlementInfoConfirm(_req, 0))
+
+    def OnRspSettlementInfoConfirm(
+            self,
+            pSettlementInfoConfirm: tdapi.CThostFtdcSettlementInfoConfirmField,
+            pRspInfo: tdapi.CThostFtdcRspInfoField,
+            nRequestID: int,
+            bIsLast: bool,
+    ):
+        """投资者结算结果确认响应"""
+        if not self._check_rsp(pRspInfo, pSettlementInfoConfirm):
+            return
+
+    def qry_instrument(self):
+        """请求查询合约"""
+        print("> 请求查询合约")
+        self.print("发送请求")
+        _req = tdapi.CThostFtdcQryInstrumentField()
+        # 填空可以查询到所有合约
+        # _req.ExchangeID = ''
+        # _req.ProductID = ''
+        # _req.InstrumentID = ''
+        self._check_req(_req, self._api.ReqQryInstrument(_req, 0))
+
+    def OnRspQryInstrument(
+            self,
+            pInstrument: tdapi.CThostFtdcInstrumentField,
+            pRspInfo: tdapi.CThostFtdcRspInfoField,
+            nRequestID: int,
+            bIsLast: bool,
+    ):
+        """请求查询合约响应"""
+        if not self._check_rsp(pRspInfo, pInstrument):
+            return
+        self._instrument = True
+
+        if bIsLast:
+            self._instrument = False
+
+    def wait(self):
+        # 阻塞 等待
+        print("-------------------------------- 按任意键退出 trader api demo ")
+        input()
+
+        self.release()
 
 
-if __name__ == '__main__':
-    td_front = random.choice(('tcp://180.168.146.187:10130',
-                              'tcp://180.168.146.187:10201',
-                              'tcp://180.168.146.187:10202',
-                              'tcp://180.168.146.187:10203'))
+if __name__ == "__main__":
+    spi = CTdSpiImpl(
+        config.fronts["电信1"]["td"],
+        config.user,
+        config.password,
+        config.authcode,
+        config.appid,
+        config.broker_id,
+    )
 
-    # user/password 需要更换为自己的
-    user = 'user'
-    password = 'password'
-    brokerid = '9999'
-    authcode = '0000000000000000'
-    appid = 'simnow_client_test'
+    # 等待登录成功
+    while True:
+        time.sleep(1)
+        if spi.is_login:
+            break
 
-    td_api = tdapi.CThostFtdcTraderApi.CreateFtdcTraderApi(user)
-    print("ApiVersion: ", td_api.GetApiVersion())
-    td_spi = CTdSpiImpl(td_api)
-    td_api.RegisterSpi(td_spi)
-    td_api.SubscribePrivateTopic(tdapi.THOST_TERT_QUICK)
-    td_api.SubscribePublicTopic(tdapi.THOST_TERT_QUICK)
-    td_api.RegisterFront(td_front)
-    td_api.Init()
+    # spi.settlement_info_confirm()
+    spi.qry_instrument()
 
-    print("press Enter key to exit ...")
-    input()
+    spi.wait()
